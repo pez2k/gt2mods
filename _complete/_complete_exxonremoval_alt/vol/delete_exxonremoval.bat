0816b9621c2c4f9fd0acb2b617f2ea83d703abd0
pez2k <pez2k@pez2k.net>
2021-10-13 00:56:10 +0100

    Exxon removal: Add script to delete the now unused bodies.

---
+++ _complete/_complete_exxonremoval_alt/vol/delete_exxonremoval.bat
@@ -0,0 +1,12 @@
+del files\carobj\fp2xr.*
+del files\carobj\fp4un.*
+del files\carobj\fp4ur.*
+del files\carobj\fp27n.*
+del files\carobj\fp27r.*
+del files\carobj\t2h8r.*
+del files\carobj\t2h9r.*
+del files\carobj\t22hn.*
+del files\carobj\t22hr.*
+del files\carobj\us35n.*
+del files\carobj\us35r.*
+del files\carobj\us50n.*
