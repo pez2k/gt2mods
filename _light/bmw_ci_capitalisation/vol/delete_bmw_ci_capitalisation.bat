2de44c7ec1b924f8f4ba833060d8b47ef3fb0263
pez2k <pez2k@pez2k.net>
2022-05-14 17:46:18 +0100

    BMW Ci capitalisation fix: Fix the BMW coupes using a lower-case 'ci' instead of 'Ci' for all US / JP logos, all RM logos, and the US dealership.

---
+++ _light/bmw_ci_capitalisation/vol/delete_bmw_ci_capitalisation.bat
@@ -0,0 +1,8 @@
+del files\carlogo\gb0cnp--.tim
+del files\carlogo\gb0cnq--.tim
+del files\carlogo\gb84np--.tim
+del files\carlogo\gb84nq--.tim
+del files\carlogo\gbc3np--.tim
+del files\carlogo\gbc3nq--.tim
+del files\carlogo\gbc8np--.tim
+del files\carlogo\gbc8nq--.tim
