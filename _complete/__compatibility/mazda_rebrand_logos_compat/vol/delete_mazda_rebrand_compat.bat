bdefcb32d7c299f77250a1dbeeac9bdee11c2af8
pez2k <pez2k@pez2k.net>
2023-01-30 11:45:26 +0000

    Mazda rebrand: Fix the Roadster SCV still having regional logos.

---
+++ _complete/__compatibility/mazda_rebrand_logos_compat/vol/delete_mazda_rebrand_compat.bat
@@ -0,0 +1,8 @@
+del files\carlogo\a-iann--.tim
+del files\carlogo\a-iano--.tim
+del files\carlogo\a-ianp--.tim
+del files\carlogo\a-ianq--.tim
+del files\carlogo\a-iarn--.tim
+del files\carlogo\a-iaro--.tim
+del files\carlogo\a-iarp--.tim
+del files\carlogo\a-iarq--.tim
