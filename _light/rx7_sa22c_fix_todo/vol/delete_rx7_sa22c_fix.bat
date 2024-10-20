1120fcac79bbbb91923e27c8f5fbbf67336997cb
pez2k <pez2k@pez2k.net>
2024-10-20 01:41:35 +0100

    RX-7 SA22C fix: Rework the physics for the early RX-7 to change it from a 165ps '83 GT-Turbo to a 130ps '79 SE-Limited to match the model, and add a Series 1 RX-7 logo to replace the FC logo used. This still needs a yellow colour to replace black, and silver wheels.

---
+++ _light/rx7_sa22c_fix_todo/vol/delete_rx7_sa22c_fix.bat
@@ -0,0 +1,8 @@
+del files\carlogo\a222nn--.tim
+del files\carlogo\a222no--.tim
+del files\carlogo\a222np--.tim
+del files\carlogo\a222nq--.tim
+del files\carlogo\a222rn--.tim
+del files\carlogo\a222ro--.tim
+del files\carlogo\a222rp--.tim
+del files\carlogo\a222rq--.tim
